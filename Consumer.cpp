0102755a89e95af82f672b56b810eedf58cdbd3d
Alex Turner <alexturner609@gmail.com>
2021-06-24 14:53:17 -0400

    Filled out the 'produce' and 'consume' functions
    Forgive me if what I have is incorrect. My IDE was saying that there were errors for even code that was already there. I believe my syntax should be correct based off the documentation I found online.

--- Consumer.cpp
+++ Consumer.cpp
@@ -1,5 +1,6 @@
 #include "ItemQueue.h"
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Consumer {
@@ -10,7 +11,9 @@ class Consumer {
         }
 
         void consume() {
-
+            Item thing = queue.take();
+            //The java version uses a logger here, but I don't think that exists in C++, so I figure a cout is the next best thing
+            cout << "Consumer " << name << " consume item " << thing.getId() << " produced by " << thing.getProducer() << endl;
         }
 
     private:
--- Producer.cpp
+++ Producer.cpp
@@ -1,5 +1,13 @@
 #include "ItemQueue.h"
+//for the sleep function--based off what i found online you need to make sure it works in different OSs
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 #include <string>
+//for the rand function
+#include <stdlib.h>
 using namespace std;
 
 class Producer {
@@ -10,7 +18,10 @@ class Producer {
         }
 
         void produce() {
-
+            Item thing = new Item(name, itemId++);
+            queue.put(thing);
+            //in the java it sleeps for a random amount of time, we can change the amount if needed
+            sleep(rand() % 5 + 1);
         }
 
     private:
