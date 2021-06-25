401386495f80892782fe861e8528fc19d5211b38
Nico Aleman <nicoaleman1@hotmail.com>
2021-06-25 07:46:32 -0400

    Fixed Producer compilation errors
    - Replaces sleep() with sleep_for()
    - Fixed Item initialization in produce()

--- Producer.cpp
+++ Producer.cpp
@@ -1,13 +1,9 @@
 #include "ItemQueue.h"
-//for the sleep function--based off what i found online you need to make sure it works in different OSs
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
 #include <string>
 //for the rand function
 #include <stdlib.h>
+#include <thread>
+#include <chrono> 
 using namespace std;
 
 class Producer {
@@ -18,10 +14,10 @@ class Producer {
         }
 
         void produce() {
-            Item thing = new Item(name, itemId++);
+            Item thing = Item(name, itemId++);
             queue.put(thing);
             //in the java it sleeps for a random amount of time, we can change the amount if needed
-            sleep(rand() % 5 + 1);
+            this_thread::sleep_for (chrono::seconds(rand() % 5 + 1));
         }
 
     private:
