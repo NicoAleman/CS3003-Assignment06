9e048c1b19e4b615ee6a85689e88693a8a4a5463
Nico Aleman <nicoaleman1@hotmail.com>
2021-06-27 19:25:25 -0400

    Moved main() code out of placeholder block
    Placeholder block is just there for feedback that the code ran successfully for now

--- main.cpp
+++ main.cpp
@@ -5,23 +5,24 @@
 using namespace std;
 
 main() {
-    // *PLACEHOLDER* //
-    cout << "Press ENTER to continue....." << endl;
-    cin.ignore(1);
-    ItemQueue queue = ItemQueue();
+    ItemQueue queue = ItemQueue(); // Initialize Queue
+
+    // Initialize Producers and Consumers
     Producer produce1 = Producer("produce1",queue);
     Producer produce2 = Producer("produce2",queue);
     Consumer consume1 = Consumer("consume1",queue);
     Consumer consume2 = Consumer("consume2",queue);
     Consumer consume3 = Consumer("consume3",queue);
+
+    // Start running Producers and Consumers
     produce1.produce();
     produce2.produce();
     consume1.consume();
     consume2.consume();
     consume3.consume();
 
-
-
-
+    // *PLACEHOLDER* //
+    cout << "Press ENTER to continue....." << endl;
+    cin.ignore(1);
     // ///////////// //
 }
